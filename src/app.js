1e9005b0358f6cbfa5d733f457e938855edd4c3b
arigree <arigree@iu.edu>
2024-11-25 03:09:39 -0500

    Fixed login, create account, and signout functionality. Worked on some styling for account. Fixed firebase auth

--- src/app.js
+++ src/app.js
@@ -1,39 +1,160 @@
 import * as $ from "jquery";
+import { signUserUp, signUserOut, signUserIn } from "./model";
 import { changePage } from "../src/model.js";
 
-const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const nav = document.querySelector(".nav");
+const userRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
 
-function route() {
-  let hashTag = window.location.hash || "#";
-  let pageID = hashTag.replace("#", "");
-  console.log("Routing to:", pageID);
-
-  changePage(pageID);
-}
 function initSite() {
   $(window).on("hashchange", route);
   route();
 }
 
-hamburgerMenu.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
+function setupMenu() {
+  const hamburgerMenu = document.querySelector(".hamburgerMenu");
+  const nav = document.querySelector(".nav");
 
-function initListeners() {
-  $(document).on("click", "a", function (e) {
-    let id = e.currentTarget.id;
-    console.log(id);
-    window.location.hash = id;
+  hamburgerMenu.addEventListener("click", () => {
     nav.classList.toggle("active");
   });
 
-  $(".nav").on("click", function () {
+  $(document).on("click", ".nav", () => {
     nav.classList.toggle("active");
   });
 }
 
+function initListeners() {
+  console.log("Initializing listeners...");
+
+  if ($("#submit").length) {
+    console.log("Sign-up button found");
+    $(document).on("click", "#submit", (e) => {
+      e.preventDefault();
+      console.log("Sign-up button clicked");
+      const firstName = $("#fName").val();
+      const lastName = $("#lName").val();
+      const email = $("#email").val();
+      const password = $("#password").val();
+      signUserUp(firstName, lastName, email, password);
+    });
+  } else {
+    console.log("Sign-up button not found");
+  }
+
+  if ($("#siSubmit").length) {
+    console.log("Login button found");
+    $(document).on("click", "#siSubmit", (e) => {
+      e.preventDefault();
+      console.log("Login button clicked");
+      const siEmail = $("#siEmail").val();
+      const siPassword = $("#siPassword").val();
+      signUserIn(siEmail, siPassword);
+    });
+  } else {
+    console.log("Login button not found");
+  }
+
+  $(document).on("click", "#so", () => {
+    signUserOut();
+  });
+
+  $(document).on("click", "#ingredBtn", function () {
+    const currentIngredCount = $(".ingreds input").length + 1;
+    $(".ingreds").append(
+      `<input type="text" id="ingred${currentIngredCount}" placeholder="ingredient #${currentIngredCount}" />`
+    );
+  });
+
+  $(document).on("click", "#instructBtn", function () {
+    const currentInstructCount = $(".instructs input").length + 1;
+    $(".instructs").append(
+      `<input type="text" id="instruct${currentInstructCount}" placeholder="instruction #${currentInstructCount}" />`
+    );
+  });
+
+  $(document).on("click", "#submitBtn", function () {
+    console.log("Submit button clicked");
+
+    let recipe = {
+      recipeName: $("#recipeName").val(),
+      recipeImageURL: $("#imageURL").val(),
+      ingredients: [],
+      instructions: [],
+    };
+
+    $(".ingreds input").each(function () {
+      const value = $(this).val();
+      if (value) recipe.ingredients.push(value);
+    });
+
+    $(".instructs input").each(function () {
+      const value = $(this).val();
+      if (value) recipe.instructions.push(value);
+    });
+
+    userRecipes.push(recipe);
+    localStorage.setItem("recipes", JSON.stringify(userRecipes));
+
+    alert("Recipe submitted successfully!");
+    $(".form input").val("");
+    console.log("Updated recipes:", userRecipes);
+  });
+}
+
+function route() {
+  const hashTag = window.location.hash || "#";
+  const pageID = hashTag.replace("#", "");
+
+  console.log("Routing to:", pageID);
+
+  if (pageID) {
+    $.get(`pages/${pageID}.html`, function (data) {
+      $("#app").html(data);
+      initListeners();
+
+      if (pageID === "create") {
+        initListeners();
+      } else if (pageID === "showAllRecipes") {
+        renderRecipes();
+      }
+    });
+  } else {
+    $.get(`pages/create.html`, function (data) {
+      $("#app").html(data);
+      initListeners();
+    });
+  }
+
+  changePage(pageID);
+}
+
+function renderRecipes() {
+  const recipes = JSON.parse(localStorage.getItem("recipes")) || [];
+  let recipeHTML = "";
+
+  if (recipes.length > 0) {
+    recipes.forEach((recipe) => {
+      recipeHTML += `
+        <div class="recipe">
+          <div class="recipeImageHolder">
+            <img src="${recipe.recipeImageURL}" alt="Recipe Image" />
+          </div>
+          <div class="recipeDescription">
+            <h2>${recipe.recipeName}</h2>
+            <p><strong>Ingredients:</strong> ${recipe.ingredients.join(", ")}</p>
+            <p><strong>Instructions:</strong> ${recipe.instructions.join(", ")}</p>
+          </div>
+        </div>`;
+    });
+  } else {
+    recipeHTML = "<p>No recipes available! Please create one first.</p>";
+  }
+
+  $("#showAllRecipes").html(recipeHTML);
+}
+
 $(document).ready(function () {
+  console.log("Document is ready");
   initSite();
-  initListeners();
+  setupMenu();
 });
+
--- src/model.js
+++ src/model.js
@@ -1,29 +1,91 @@
 import * as $ from "jquery";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { app } from "./firebaseConfig";
+
+const auth = getAuth(app);
 
 export function changePage(pageName) {
-    console.log(pageName);
-    if (pageName) {
-        $.get("../../pages/" + pageName + ".html")
-            .done((data) => {
-                $("#app").html(data);
-                
-            })
-            .fail((error) => {
-                console.log("error " + error);
-                alert("Error loading page: " + error.statusText);
-                $.get("../../pages/home.html", (data) => {
-                    $("#app").html(data);
-                }).fail((err) => {
-                    console.log("Error loading home: " + err);
-                });
-            });
-    } else {
+  console.log(pageName);
+  if (pageName) {
+    $.get("../../pages/" + pageName + ".html")
+      .done((data) => {
+        $("#app").html(data);
+      })
+      .fail((error) => {
+        console.log("error " + error);
+        alert("Error loading page: " + error.statusText);
         $.get("../../pages/home.html", (data) => {
-            $("#app").html(data);
-        }).fail((error) => {
-            console.log("error " + error);
-            alert("Error loading home: " + error.statusText);
+          $("#app").html(data);
+        }).fail((err) => {
+          console.log("Error loading home: " + err);
         });
-    }
+      });
+  } else {
+    $.get("../../pages/home.html", (data) => {
+      $("#app").html(data);
+    }).fail((error) => {
+      console.log("error " + error);
+      alert("Error loading home: " + error.statusText);
+    });
+  }
 }
 
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("User is signed in");
+      $("#login").text("Sign Out");
+      $("#login").attr("href", "#");
+      $("#login").off("click").on("click", () => signUserOut()); 
+      $(".footer-nav a:contains('Create Recipe')").show();
+      $(".footer-nav a:contains('Your Recipes')").show();
+      $(".nav a:contains('Create Recipe')").show();
+      
+    } else {
+      console.log("User is signed out");
+      $("#login").text("LogIn");
+      $("#login").attr("href", "#account");
+      $("#login").off("click").on("click", () => window.location.hash = "#account");
+      $(".footer-nav a:contains('Create Recipe')").hide();
+      $(".footer-nav a:contains('Your Recipes')").hide();
+      $(".nav a:contains('Create Recipe')").hide();
+    }
+  });
+
+export function signUserUp(fn, ln, email, password) {
+  // console.log("model.js" + fn, ln, email, password);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      console.log("User Created");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function signUserOut() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        console.log("User signed out");
+      })
+      .catch((error) => {
+        console.log("Error signing out:", error);
+      });
+  }
+
+export function signUserIn(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        window.location.hash = "#yourRecipes";
+        console.log("User Signed in");
+      })
+      .catch((error) => {
+        console.log("Error during sign-in:", error.message);
+      });
+  }
