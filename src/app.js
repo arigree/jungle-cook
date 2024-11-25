d522a8dd98f257a54a97072a0abc20265791eb40
arigree <arigree@iu.edu>
2024-11-24 20:59:57 -0500

    initial setup, new github

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import * as $ from "jquery";
+
+function initListeners() {
+    console.log("initListeners");
+}
+
+$(document).ready(function (){
+    initListeners();
+});
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhDvs-raN3MvAkPxoojyixOvNCIkrx6iw",
+  authDomain: "jungle-cook-70632.firebaseapp.com",
+  projectId: "jungle-cook-70632",
+  storageBucket: "jungle-cook-70632.firebasestorage.app",
+  messagingSenderId: "52316013857",
+  appId: "1:52316013857:web:98c0c208a256224f556a60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
